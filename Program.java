c8070f01364b7192e2dbab8fb6b93b0af2e6e4c1
ame1994 <andrei.merinov@mail.ru>
2019-02-26 22:09:50 +0300

    Lesson4

--- Program.java
+++ Program.java
@@ -1,74 +1,20 @@
+import java.util.Iterator;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println(reverse("Some string."));
-        testDeque();
-    }
-
-    private static void testDeque() {
-        MyArrayDeque<Integer> deque = new MyArrayDeque<>();
-        System.out.println(deque);
-        deque.insertLeft(1);
-        System.out.println(deque);
-        deque.insertLeft(2);
-        System.out.println(deque);
-        deque.insertLeft(3);
-        System.out.println(deque);
-        deque.insertLeft(4);
-        System.out.println(deque);
-        deque.insertLeft(5);
-        System.out.println(deque);
-        deque.insertLeft(6);
-        System.out.println(deque);
-        deque.removeLeft();
-        System.out.println(deque);
-        deque.removeLeft();
-        System.out.println(deque);
-        deque.removeLeft();
-        System.out.println(deque);
-        deque.removeLeft();
-        System.out.println(deque);
-        deque.removeLeft();
-        System.out.println(deque);
-        deque.removeLeft();
-        System.out.println(deque);
-        System.out.println();
-        System.out.println();
-
-        deque.insertRight(1);
-        System.out.println(deque);
-        deque.insertRight(2);
-        System.out.println(deque);
-        deque.insertRight(3);
-        System.out.println(deque);
-        deque.insertRight(4);
-        System.out.println(deque);
-        deque.insertRight(5);
-        System.out.println(deque);
-        deque.insertRight(6);
-        System.out.println(deque);
-        deque.removeRight();
-        System.out.println(deque);
-        deque.removeRight();
-        System.out.println(deque);
-        deque.removeRight();
-        System.out.println(deque);
-        deque.removeRight();
-        System.out.println(deque);
-        deque.removeRight();
-        System.out.println(deque);
-        deque.removeRight();
-        System.out.println(deque);
-    }
-
-    private static String reverse(String str) {
-        MyArrayStack<Character> chars = new MyArrayStack<>();
-        for (int i = 0; i < str.length(); i++) {
-            chars.push(str.charAt(i));
+        MyLinkedList<Integer> list = new MyLinkedList<>();
+        for (int i = 0; i < 6; i++) {
+            list.insertFirst(i);
         }
-        StringBuilder newStr = new StringBuilder();
-        while (!chars.isEmpty()) {
-            newStr.append(chars.pop());
+
+        for (Integer item: list) {
+            System.out.println(item);
         }
-        return newStr.toString();
+
+        System.out.println("Through iterator on direct.");
+        Iterator<Integer> iter = list.iterator();
+        while (iter.hasNext()) {
+            System.out.println(iter.next());
+        }
     }
 }
