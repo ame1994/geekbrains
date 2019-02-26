7979c982583102acf299f3fdfeddb60032674b2c
ame1994 <andrei.merinov@mail.ru>
2019-02-26 21:36:35 +0300

    Lesson4

---
+++ Lesson4/MyLinkedStack.java
@@ -0,0 +1,28 @@
+public class MyLinkedStack<T> {
+    private MyLinkedList<T> stack = new MyLinkedList<>();
+
+    public void push(T item) {
+        stack.insertFirst(item);
+    }
+
+    public T pop() {
+        return stack.deleteFirst();
+    }
+
+    public T peek() {
+        return stack.getFirst();
+    }
+
+    public int size() {
+        return stack.size();
+    }
+
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        return stack.toString();
+    }
+}
